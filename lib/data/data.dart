5f8f6b37dfd67a690f815fb924d42a78f5d8ff2a
Miles639 <mpearson6@twu.edu>
2025-02-17 11:10:30 -0600

    MiniApp1

---
+++ lib/data/data.dart
@@ -0,0 +1,69 @@
+
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> transactionsData =[
+  {
+    'icon': Icon(Icons.restaurant_rounded, color:Colors.white),
+    'color':Colors.yellow[700],
+    'name':'Restrant',
+    'totalAmount':'-\$45.00',
+    'date':'Today',
+    'description':'Tuscan Itailian Restrant',
+  },
+    {
+    'icon': Icon(Icons.shopify, color:Colors.white),
+    'color':Colors.purple,
+    'name':'Shopping',
+    'totalAmount':'-\$230.00',
+    'date':'Today',
+    'description':'H&M'
+  },
+      {
+    'icon': Icon(Icons.health_and_safety, color:Colors.white),
+    'color':Colors.green,
+    'name':'Health',
+    'totalAmount':'-\$79.00',
+    'date':'Yesterday',
+    'description':'Walgreens Pharmacy'
+  },
+  {
+    'icon': Icon(Icons.airplane_ticket, color:Colors.white),
+    'color':Colors.blue,
+    'name':'Travel',
+    'totalAmount':'-\$350.00',
+    'date':'Yesterday',
+    'description':'American Airlines'
+  },
+  {
+    'icon': Icon(Icons.house, color:Colors.white),
+    'color':Colors.brown,
+    'name':'Rent/Morgage',
+    'totalAmount':'-\$1,100.00',
+    'date':'1/31/2025',
+    'description':'Highland Lake Apartments Leasing Office'
+  },
+    {
+    'icon': Icon(Icons.food_bank ,color:Colors.white),
+    'color':Colors.blue[100],
+    'name':'Grocery',
+    'totalAmount':'-\$125.00',
+    'date':'2/06/2025',
+    'description':'Walmart'
+  },
+      {
+    'icon': Icon(Icons.car_rental ,color:Colors.white),
+    'color':Colors.grey[300],
+    'name':'Transportation',
+    'totalAmount':'-\$365.00',
+    'date':'02/9/2025',
+    'description':'Honda Finacning'
+  },
+  {
+    'icon': Icon(Icons.subscriptions, color:Colors.white),
+    'color':Colors.red,
+    'name':'Subscriptions',
+    'totalAmount':'-\$10.00',
+    'date':'Yesterday',
+    'description':'Netflix Subscription',
+  },
+];
